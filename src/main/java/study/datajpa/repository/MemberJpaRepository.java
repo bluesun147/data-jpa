69a5458f7754f4929802adc5b117175b0784deba
HaeChan Kim <bluesun147@naver.com>
2022-10-02 23:41:08 +0900

    순수 JPA 기반 리포지토리 만들기

--- src/main/java/study/datajpa/repository/MemberJpaRepository.java
+++ src/main/java/study/datajpa/repository/MemberJpaRepository.java
@@ -5,6 +5,8 @@ import study.datajpa.entity.Member;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import java.util.List;
+import java.util.Optional;
 
 @Repository
 public class MemberJpaRepository {
@@ -17,6 +19,30 @@ public class MemberJpaRepository {
         return member;
     }
 
+    public void delete(Member member) {
+        em.remove(member);
+    }
+
+    // 전체 조회
+    public List<Member> findAll() {
+        // jpql -> 테이블 대상이 아닌 객체 대상 쿼리
+        return em.createQuery("select m from Member m", Member.class)
+                .getResultList();
+    }
+
+    public Optional<Member> findById(Long id) {
+        Member member = em.find(Member.class, id);
+        // null일 수도 있다는 옵션으로 반환. 널일수도 아닐수도 있음
+        // optional은 java 8 기본적인 기능. 알아두기
+        return Optional.ofNullable(member);
+    }
+
+    public long count() {
+        return em.createQuery("select count(m) from Member m", Long.class)
+                .getSingleResult();
+    }
+
+    // 하나 조회
     public Member find(Long id) {
         return em.find(Member.class, id);
     }
