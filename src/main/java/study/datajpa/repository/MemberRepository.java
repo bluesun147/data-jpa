a812d407dea17faacccb9b7ff85e829ff4dc0eb6
HaeChan Kim <bluesun147@naver.com>
2022-10-03 17:27:06 +0900

    공통 인터페이스 적용/분석

--- src/main/java/study/datajpa/repository/MemberRepository.java
+++ src/main/java/study/datajpa/repository/MemberRepository.java
@@ -4,10 +4,16 @@ package study.datajpa.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import study.datajpa.entity.Member;
 
+import java.util.List;
+
 // 인터페이스
 // 구현체 없는데 어떻게 동작할까 ??
 // 스프링 data jpa가 인터페이스 보고 프록시 객체 (구현체) 알아서 넣음
 // @Repository 생략 가능
+
+// <엔티티, 엔티티에 매핑된 pk 타입>
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
+    // 쿼리 메소드 기능
+    List<Member> findByUsername(String username);
 }
